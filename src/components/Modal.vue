5e4c4b5f847f2a2b03e786d6c3f238cb1eb58332
jae woo Jung <awfjol2008@naver.com>
2022-07-19 23:57:06 +0900

    update

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,18 +3,20 @@
         <!-- Modal -->
         <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
+                <div class="modal-content text-center">
                     <div class="modal-header">
                         <h5 class="modal-title" id="myModalLabel">{{ $store.state.roomList[0].title }}</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
+                        <img :src="$store.state.roomList[0].image" class="w-100 mb-4">
                         {{ $store.state.roomList[0].content }} <br>
                         {{ $store.state.roomList[0].price }} 원
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                    <div class="modal-footer row">
+                        <div class="col text-center">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
